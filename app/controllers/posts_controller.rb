d665addf71b7f8c028aa54cd4a1ddc59bdbc1420
nousi <nousidayo@gmail.com>
2020-10-28 09:07:52 +0900

    Add includes(:user) to @posts
    N+1問題を発生させないために追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all.order(created_at: "DESC")
+    @posts = Post.all.order(created_at: "DESC").includes(:user)
   end
 end
