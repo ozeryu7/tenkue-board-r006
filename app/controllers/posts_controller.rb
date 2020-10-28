d6f399146d932eedea1ca33e0927dcff354ca57c
tattu501 <61368201+tattu501@users.noreply.github.com>
2020-10-29 00:25:13 +0900

    posts_controller.rbにnewアクションを追加
    postの登録機能を実装するため、newアクションを追加した

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
+  def new
+    @post = Post.new
+  end
   
   def index
     @posts = Post.all.order(created_at: "DESC").includes(:user)
