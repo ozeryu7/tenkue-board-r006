f3216dc9c16e2474478c4071f03bfba981341495
nousi <nousidayo@gmail.com>
2020-10-31 16:11:11 +0900

    Revert "チームメンバーのpullを修正"
    This reverts commit 67e59edafbd8d0a6db44411b83754e7fc3883674.

--- db/migrate/20201024140953_create_posts.rb
+++ db/migrate/20201024140953_create_posts.rb
@@ -2,11 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.text :content
-<<<<<<< HEAD
       t.references :user, foreign_key: true
-=======
-      # t.references :user, foreign_key: true
->>>>>>> parent of f5e6dd0... Merge branch 'origin/user.devise' into posts.create
       t.timestamps
     end
   end
