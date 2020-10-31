306c63810c8dc78fd1d458c178a81f16848a9c4f
tattu501 <61368201+tattu501@users.noreply.github.com>
2020-10-31 14:57:40 +0900

    posts.controler.rbにbefore_actionのauthenticate_admin!を追加
    投稿者以外が編集ページにアクセスしようとすると、投稿一覧ページへ遷移するよう、authenticate_admin!メソッドをbefore_actionとして実装した。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  
+  before_action :authenticate_admin!, except: [:index, :show, :new, :create]
+
   def index
     @posts = Post.includes(:user).all.order("created_at DESC")
   end
@@ -16,16 +17,32 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
-      flash[:success] = "登録しました"
+      flash[:notice] = "登録しました"
       redirect_to root_url
     else
       render 'posts/new'
     end
   end
-  
+
+  def edit
+
+  end
+
+  def destroy 
+
+  end
 
   private
     def post_params
       params.require(:post).permit(:content).merge(user_id: current_user.id)
+    end
+
+    def authenticate_admin!
+      @post = Post.includes(:user).find(params[:id])
+      
+      if @post.user_id != current_user.id
+        flash[:notice] = "権限はありません"
+        redirect_to root_url
+      end
     end
 end
