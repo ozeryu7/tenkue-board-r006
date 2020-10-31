67e59edafbd8d0a6db44411b83754e7fc3883674
nousi <nousidayo@gmail.com>
2020-10-31 15:56:24 +0900

    チームメンバーのpullを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,4 @@ class User < ApplicationRecord
   has_many :posts
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 8, maximum: 32 }
 end
