200fc39d4c482fe7f98682c67e78b49efeb75443
tattu501 <61368201+tattu501@users.noreply.github.com>
2020-11-03 19:02:59 +0900

    comments_controller.rbにedit/update/destroyアクションを追加
    commentsコントローラにedit/update/destroyの3アクションを追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,8 @@
 class CommentsController < ApplicationController
+  def edit
+    
+  end
+
   def create
     @comment = current_user.comments.new(comment_params)
     if @comment.save
@@ -9,8 +13,25 @@ class CommentsController < ApplicationController
     end
   end
 
+  def update
+
+  end
+
+  def destroy
+
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:comment, :user_id, :post_id)
+    end
+
+    def authenticate_admin!
+      @post = Post.find(params[:id])
+      
+      if @post.user_id != current_user.id
+        flash[:notice] = "権限はありません"
+        redirect_to root_url
+      end
     end
 end
