b2073fef69c48e85ddf36b49bcf524bce5d4ce9a
tattu501 <61368201+tattu501@users.noreply.github.com>
2020-11-03 09:38:46 +0900

    posts.controller.rbを修正
    before_action :authenticate_admin!のアクション指定をexceptからonlyに変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :authenticate_admin!, except: [:index, :show, :new, :create]
+  before_action :authenticate_admin!, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.includes(:user).all.order("created_at DESC")
