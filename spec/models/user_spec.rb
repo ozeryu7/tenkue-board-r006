99272b1609ed1f1bed46e1de61bf4409e3d6393d
nousi <nousidayo@gmail.com>
2020-11-04 20:43:58 +0900

    Add it "is valid" in user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,10 @@ RSpec.describe User do
 
   describe '#validation' do
     context 'can save' do
-
+      it "is valid with a name, email, password, password_confirmation" do
+        user = build(:user)
+        expect(user).to be_valid
+      end
     end
     context 'can not save' do
 
