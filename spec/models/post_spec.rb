9eace12fa1f2aa98aab95e759337eb829760cdaa
nousi <nousidayo@gmail.com>
2020-11-04 20:57:31 +0900

    Add it "is valid" in post_spec.rb
    バリデーションが成功した場合のテストを実行

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,11 +3,14 @@ require 'rails_helper'
 RSpec.describe Post do
   describe '#validation' do
     context 'can save' do
+      it "is valid with a content, user_id" do
+        post = build(:post)
+        expect(post).to be_valid
+      end
     
     end
 
     context 'can not save' do
-      
     end
   end
 end
