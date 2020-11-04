e637eb047c9a43910a751cddb34fbca7c157746c
nousi <nousidayo@gmail.com>
2020-11-04 20:36:02 +0900

    nicknameをnameに更新
    nicknameは存在しないカラム名なので、nameに更新

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory  :user do
-    nickname                  {"name"}
+    name                      {"name"}
     sequence(:email)          {Faker::Internet.email}
     password                  {"00000000"}
     password_confirmation     {"00000000"}
