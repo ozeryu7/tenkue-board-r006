a843f761361d34f99713e9e8709308565556d288
nousi <nousidayo@gmail.com>
2020-11-04 14:26:25 +0900

    before_action :set_post を追加
    @post = Post.find(params[:post_id])をどのメソッドでも記述するため、set_postで@postを定義し、controllerの記述を短縮した

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,17 @@
 class LikesController < ApplicationController
+  before_action :set_post
+
   def create
-    @post = Post.find(params[:post_id])
     @like = Like.create(user_id: current_user.id, post_id: @post.id)
   end
 
   def destroy
-    @post = Post.find(params[:post_id])
     @like = Like.find_by(user_id: current_user.id, post_id: @post.id)
     @like.destroy
   end
+
+  private
+  def set_post
+    @post = Post.find(params[:post_id])
+  end 
 end
