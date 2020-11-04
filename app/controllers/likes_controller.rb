1291d05a3838d8aab6ed53464cddf99f35cb0a07
nousi <nousidayo@gmail.com>
2020-11-04 14:18:15 +0900

    like.createを追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,7 @@
 class LikesController < ApplicationController
   def create
-    
+    @post = Post.find(params[:post_id])
+    @like = Like.create(user_id: current_user.id, post_id: @post.id)
   end
 
   def destroy
