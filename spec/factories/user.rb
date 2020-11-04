65622f0bbfa7b302afaa2f2f399cbd0fbaf0116c
nousi <nousidayo@gmail.com>
2020-11-04 20:43:02 +0900

    emailを指定　in FactoryBot of user

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory  :user do
     name                      {"name"}
-    sequence(:email)          {Faker::Internet.email}
+    email                     {"test@test"}
     password                  {"00000000"}
     password_confirmation     {"00000000"}
   end
