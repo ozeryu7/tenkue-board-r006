417e326a74d548c3ba1b03b98b27c15b4adc423b
nousi <nousidayo@gmail.com>
2020-11-04 13:46:07 +0900

    いいね判定メソッドalready_liked?を定義
    userがすでにいいねしているか判定する際に利用する。
    この機能で重複いいねを防ぐために利用する。

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, exclusion: { in: [/^[ぁ-んァ-ヶー一-龠]+$/] }, presence: true, uniqueness: true
   validates :password, format: { with: /\A[a-z0-9]+\z/i }, presence: true, length: { in: 8..32 }
+
+  def already_liked?(post)
+    self.likes.exists?(post_id: post.id)
+  end
 end
