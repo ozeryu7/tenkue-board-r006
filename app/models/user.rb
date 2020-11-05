9bcf6940a669758206973946a9d1c61693ba0c1d
nousi <nousidayo@gmail.com>
2020-11-06 00:33:06 +0900

    パスワードに半角英数字を両方含むvalidates
    半角英数字を含むことが条件

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :email, exclusion: { in: [/^[ぁ-んァ-ヶー一-龠]+$/] }, presence: true, uniqueness: true
-  validates :password, format: { with: /\A[a-z0-9]+\z/i }, presence: true, length: { in: 8..32 }
-  validates :password_confirmation, format: { with: /\A[a-z0-9]+\z/i }, presence: true, length: { in: 8..32 }
+  validates :password, format: { with: /\A(?=.&#042;?[a-z])(?=.&#042;?\d)[a-z\d]+\z/i }, presence: true, length: { in: 8..32 }
+  validates :password_confirmation, format: { with: /\A(?=.&#042;?[a-z])(?=.&#042;?\d)[a-z\d]+\z/i }, presence: true, length: { in: 8..32 }
 end
 
