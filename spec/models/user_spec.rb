76d337364686eebe51219049fea07eca3b13f03c
nousi <nousidayo@gmail.com>
2020-11-06 00:20:00 +0900

    passwordとpassword_confirmationが一致しない場合のRspecを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe User do
         expect(user.errors[:name]).to include("を入力してください")
       end
 
-      it "is invalid without a name" do
+      it "is invalid without a email" do
         user = build(:user, email: nil)
         user.valid?
         expect(user.errors[:email]).to include("を入力してください")
@@ -28,6 +28,11 @@ RSpec.describe User do
         expect(user.errors[:password]).to include("を入力してください", "は不正な値です", "は8文字以上で入力してください")
       end
 
+      it "is invalid because it doesn't match password and password_confirmation" do
+        user = build(:user, password_confirmation: 12345678)
+        user.valid?
+        expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
+      end
       
     end
   end
