60af909ead83c8c313195529a38536abe6977e76
nousi <nousidayo@gmail.com>
2020-11-06 00:42:30 +0900

    半角英数字をパスワードに含む必要があるため、修正

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory  :user do
     name                      {"name"}
     email                     {Faker::Internet.free_email}
-    password                  {"00000000"}
-    password_confirmation     {"00000000"}
+    password                  {"test0000"}
+    password_confirmation     {"test0000"}
   end
 end 
