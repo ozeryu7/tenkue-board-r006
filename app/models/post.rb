d83d5f53e408f5c7eeb573fc0f808362ab397cd1
nousi <nousidayo@gmail.com>
2020-11-06 22:13:44 +0900

    モデルのリレーションに,を追加
    ,忘れの箇所に追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :likes dependent: :destroy
+  has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
   validates :content, presence: true, length: { maximum: 140 }
 end
